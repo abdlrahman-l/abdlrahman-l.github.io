f74b3e02f2ad0e56b65fc453e88be3efa78fef21
Abdul Rahman <abdul.rahman@klikdokter.com>
2024-09-04 16:42:10 +0700

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://afiiif.github.io',
+  site: 'https://abdlrahman-l.github.io',
   integrations: [tailwind()],
 });
