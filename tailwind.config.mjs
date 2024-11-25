8008718ec2e1d891bf5768aea751e0a706a01bcb
Abdul Rahman <abdul.rahman@klikdokter.com>
2024-11-25 15:18:39 +0700

    set colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    colors: {
+      primary: '#F1F0E8',
+      secondary: '#B3C8CF',
+      tertiary: '#1C325B'
+      // ...
+    }
   },
   plugins: [],
 };
